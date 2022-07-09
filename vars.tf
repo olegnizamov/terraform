9871d195c66602544650ae12afc4750ef976bd15
Олег Низамов <35757313+olegnizamov@users.noreply.github.com>
2022-07-09 14:46:42 +0400

    vars

---
+++ vars.tf
@@ -0,0 +1,23 @@
+variable "yc_token" {
+   default = ""
+}
+
+variable "yc_cloud_id" {
+  default = ""
+}
+
+variable "yc_folder_id" {
+  default = ""
+}
+
+variable "yc_region" {
+  default = "ru-central1-a"
+}
+
+variable "yc_key_file" {
+  default = "./secrets/key.json"
+}
+
+variable "id_rsa_pub" {
+  default = ""
+}
